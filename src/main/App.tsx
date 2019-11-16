d1c02b93b4fef6edaf92d4240bac860f8ea750b1
mbajaj23 <55329699+mbajaj23@users.noreply.github.com>
2019-11-16 17:13:23 -0600

    Manan and Peter's Working Scrollable List

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import TodoControls from './components/TodoControls';
 import IncompleteTodoDisplay from './components/IncompleteTodoDisplay';
 import CompleteTodoDisplay from './components/CompleteTodoDisplay';
+import Feedback from './components/Feedback';
 import "./App.css";
+
 
 const App: React.FC = () => {
   return (
   <div className="App" style={{ display: "inline" }}>
-      <TodoControls />
-      <IncompleteTodoDisplay />
-      <CompleteTodoDisplay />
+    
+    <Feedback />
   </div> 
   );
 };
